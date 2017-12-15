534f4d9db6bb919e6e489fbf0761b57eddc0f213
aleku399 <aleku399@gmail.com>
2017-12-15 16:03:58 +0300

    table design

---
+++ packages/pdf-base/src/components/atoms/Table/index.stories.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Table from '.';
+import * as R from 'ramda';
+
+const data = [
+    {ctry: 'Uga', dev: 20},
+    {ctry: 'Moz', dev: 30},
+    {ctry: 'Ke', dev: 40}
+];
+
+storiesOf('Table', module).add('Table', () =>
+(<div style={{width: '20%', padding: '100px'}}>
+    <Table data={data}  headings={['cty', 'dev']}/>
+</div>
+));
