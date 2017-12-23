a166d465e387844dc28100d988b79027005deb9b
aleku399 <aleku399@gmail.com>
2017-12-23 13:27:12 +0300

    api corrections

---
+++ packages/ssc-pdf/src/components/southAfrica/data.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+// this file is auto generated
+
+module.exports = {
+  dvptCooperationTrend: [
+    { uid: 'ryZ-oey3iGf', year: 2015, value: 293 },
+    { uid: 'ryfbsekhjMG', year: 2014, value: 174 },
+    { uid: 'ByX-ilyhizf', year: 2013, value: 182 },
+    { uid: 'H14Wiekhozf', year: 2012, value: 176 },
+    { uid: 'ByHZjeJ3ozf', year: 2011, value: 155 },
+    { uid: 'HyLbjlk2jzz', year: 2010, value: 151 },
+    { uid: 'r1Pbolk3ozM', year: 2009, value: 193 },
+    { uid: 'B1dbsgJ2sMG', year: 2008, value: 150 },
+    { uid: 'SJtZilJhozG', year: 2007, value: 112 },
+    { uid: 'S1qWoly3oGM', year: 2006, value: 99 }
+  ]
+};
