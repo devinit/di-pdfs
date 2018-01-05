e0d5126d737cffa4c8f529d36835271e32d1d54a
aleku399 <aleku399@gmail.com>
2018-01-05 16:15:58 +0300

    south africa pdf work

---
+++ packages/ssc-api/src/modules/southAfrica/config.ts
@@ -0,0 +1,17 @@
+import * as colors from '@di-pdfs/pdf-base/lib/theme/colors';
+
+export const regionColors = {
+    'International Relations and Cooperation': colors.red,
+    'National Treasury': colors.pink,
+    'Other': colors.darkRed,
+};
+
+export const sectorColors = {
+    'ARICF expenditures (total)': colors.darkerRed,
+    'Prevention and resolution of conflicts': colors.pink,
+    'Coopertion between South Africa and other states': colors.darkRed,
+    'Humanitarian assistance': colors.red,
+    'Promotion of democracy and good governance': colors.lightPink,
+    'Socio-economic  development and integration': colors.lightRed,
+    'Human resources development': colors.lighterRed
+};
