55da294134612151ea1bd8925daccfe13e215477
epicallan <epicallan.al@gmail.com>
2018-01-07 12:24:33 +0300

    color changes

---
+++ packages/ssc-api/src/modules/southAfrica/config.ts
@@ -0,0 +1,23 @@
+import * as colors from '@di-pdfs/pdf-base/lib/theme/colors';
+
+export const departmentColors = {
+    'International Relations and Cooperation': colors.lightPurple,
+    'National Treasury': colors.lightRed,
+    'Other': colors.darkRed
+};
+
+export const regionColors = {
+    'Africa': colors.red,
+    'Latin America and the Caribbean': colors.purple,
+    'Asia': colors.orange
+};
+
+export const sectorColors = {
+    'Human resources development': colors.lighterRed,
+    'ARICF expenditures (total)': colors.pink,
+    'Coopertion between South Africa and other states': colors.darkerRed,
+    'Promotion of democracy and good governance': colors.lightRed,
+    'Prevention and resolution of conflicts': colors.lightPink,
+    'Socio-economic  development and integration': colors.red,
+    'Humanitarian assistance': colors.darkRed
+};
