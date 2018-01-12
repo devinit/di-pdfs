02d73d771ce995b4c5f8b81ecae410ac4f81f8c7
aleku399 <aleku399@gmail.com>
2018-01-12 16:33:48 +0300

    some pdf correction

--- packages/ssc-pdf/src/components/southAfrica/Bar.tsx
+++ packages/ssc-pdf/src/components/southAfrica/Bar.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import {Div} from 'glamorous';
 import charts from '@devinit/pdf-base/lib/charts';
 import data from './data';
+import {reverse} from 'ramda';
 
 const options = {
-    data: data.dvptCooperationTrend,
+    data: reverse(data.dvptCooperationTrend),
     height: '180px',
     config: {
         linearAxis: {
