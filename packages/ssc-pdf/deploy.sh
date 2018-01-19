6b597a7722fc38f0bc788af474cebd8bfef9564c
epicallan <epicallan.al@gmail.com>
2018-01-19 16:43:24 +0300

    add deploy script

--- packages/ssc-pdf/deploy.sh
+++ packages/ssc-pdf/deploy.sh
@@ -3,11 +3,13 @@ cd ~/new-pdfs/di-pdfs
 
 git stash && git pull origin master # if this is first time deploy then use git clone
 
-docker stop ssc-pdf && docker rm ssc-pdf \
+cd packages/ssc-pdf
+
+docker stop ssc-pdf-app && docker rm ssc-pdf-app \
 
     docker build -t ssc-pdf . && \
 
-    docker run -it -d --restart unless-stopped --name ssc-pdf-app ssc-pdf
+    docker run -p 7777:7777 -it -d --restart unless-stopped --name ssc-pdf-app ssc-pdf
 
 
 
