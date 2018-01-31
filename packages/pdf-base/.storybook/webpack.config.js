d31a5e8c5169d43cf680d4d34a41dc1f5364bd2e
epicallan <epicallan.al@gmail.com>
2018-01-31 12:13:29 +0300

    minor change

--- packages/pdf-base/.storybook/webpack.config.js
+++ packages/pdf-base/.storybook/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 const SRC_PATH = path.join(__dirname, '../src');
-const STATIC_PATH = path.join(__dirname, '../static');
 
 const newRules = [
     {
